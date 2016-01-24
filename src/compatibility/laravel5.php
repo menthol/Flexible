7c4ef2c3e51fa2f6e3fc254728672334bd6c4346
nath <nath@noizers.com>
2016-01-24 15:26:30 +0100

    Change direct classes inclusions to autoloaders

--- src/compatibility/laravel5.php
+++ src/compatibility/laravel5.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once __DIR__.'/laravel5/Arrayable.php';
+spl_autoload_register(function($class) {
+    $class_map = [
+        'Menthol\Flexible\Contracts\Arrayable' => __DIR__.'/laravel5/Arrayable.php',
+    ];
+
+    if (isset($class_map[$class])) {
+        require_once $class_map[$class];
+    }
+});
