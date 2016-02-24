a7cb282b4f98e92edf6ef4739932bd7b3c3c76cc
nath <nath@noizers.com>
2016-02-24 16:06:02 +0100

    Try to push to ES

--- src/Menthol/Flexible/Jobs/ReindexJob.php
+++ src/Menthol/Flexible/Jobs/ReindexJob.php
@@ -1,6 +1,8 @@
 <?php namespace Menthol\Flexible\Jobs;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Queue\Jobs\Job;
+use Illuminate\Support\Facades\App;
 use Menthol\Flexible\Utilities\QueryHelper;
 
 /**
@@ -15,12 +17,19 @@ class ReindexJob
         foreach ($modelDefinitions as $modelName => $keys) {
 
             $models = QueryHelper::findMany($modelName, $keys);
+            $client = App::make('Elasticsearch');
+            $index_prefix = App::make('Menthol\Flexible\Config')->get('elasticsearch.index_prefix', '');
 
             foreach ($keys as $key) {
+                /** @var Model|null $model */
                 $model = $models->find($key);
                 if ($model && $model->flexibleIsIndexable()) {
-                    // Index
-                    $i = 1;
+                    $client->index([
+                        'index' => $index_prefix.str_singular($model->getTable()),
+                        'id' => $model->getKey(),
+                        'type' => str_singular($model->getTable()),
+                        'body' => [],
+                    ]);
                 } else {
                     // Remove from index
                     $i = 2;
