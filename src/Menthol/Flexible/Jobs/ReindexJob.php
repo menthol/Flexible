ac635ccae0206805116b64cc7337b24527400d40
nath <nath@noizers.com>
2016-03-09 17:33:58 +0100

    Chanck huge reindex

--- src/Menthol/Flexible/Jobs/ReindexJob.php
+++ src/Menthol/Flexible/Jobs/ReindexJob.php
@@ -17,19 +17,15 @@ class ReindexJob
     public function fire(Job $job, $modelDefinitions)
     {
         foreach ($modelDefinitions as $modelName => $keys) {
-            $chuncks = array_chunk($keys, 750);
-
-            foreach ($chuncks as $chunck) {
-                /** @var Collection $models */
-                $models = QueryHelper::findMany($modelName, $chunck, true);
-                foreach ($keys as $key) {
-                    /** @var Model|IndexableTrait|null $model */
-                    $model = $models->find($key);
-                    if ($model && $model->flexibleIsIndexable()) {
-                        ElasticSearchHelper::index($model);
-                    } else {
-                        ElasticSearchHelper::delete($modelName, $key);
-                    }
+            /** @var Collection $models */
+            $models = QueryHelper::findMany($modelName, $keys, true);
+            foreach ($keys as $key) {
+                /** @var Model|IndexableTrait|null $model */
+                $model = $models->find($key);
+                if ($model && $model->flexibleIsIndexable()) {
+                    ElasticSearchHelper::index($model);
+                } else {
+                    ElasticSearchHelper::delete($modelName, $key);
                 }
             }
         }
--- src/Menthol/Flexible/Observer.php
+++ src/Menthol/Flexible/Observer.php
@@ -6,42 +6,59 @@ use Menthol\Flexible\Utilities\RelatedModelsDiscovery;
 
 class Observer
 {
+    protected function observeRelatedModels(Model $model)
+    {
+        $model->flexibleRelatedModels = RelatedModelsDiscovery::getRelatedModels($model, $model->flexibleRelatedModels ?: []);
+    }
+
+    protected function pushReindexJob(Model $model)
+    {
+        $relatedModels = RelatedModelsDiscovery::getRelatedModels($model, $model->flexibleRelatedModels ?: []);
+
+        if (count($relatedModels, COUNT_RECURSIVE) > 750) {
+            foreach ($relatedModels as $model => $keys) {
+                foreach (array_chunk($keys, 750) as $chunkedKeys) {
+                    Queue::push('Menthol\Flexible\Jobs\ReindexJob', [$model => $chunkedKeys]);
+                }
+            }
+        } else {
+            Queue::push('Menthol\Flexible\Jobs\ReindexJob', $relatedModels);
+        }
+    }
+
     public function created(Model $model)
     {
-        Queue::push('Menthol\Flexible\Jobs\ReindexJob', RelatedModelsDiscovery::getRelatedModels($model));
+        $this->pushReindexJob($model);
     }
 
     public function updating(Model $model)
     {
-        $model->flexibleRelatedModels = RelatedModelsDiscovery::getRelatedModels($model);
+        $this->observeRelatedModels($model);
     }
 
     public function updated(Model $model)
     {
-        Queue::push('Menthol\Flexible\Jobs\ReindexJob', RelatedModelsDiscovery::getRelatedModels($model, $model->flexibleRelatedModels));
-        $model->flexibleRelatedModels = [];
+        $this->pushReindexJob($model);
     }
 
     public function deleting(Model $model)
     {
-        $model->flexibleRelatedModels = RelatedModelsDiscovery::getRelatedModels($model);
+        $this->observeRelatedModels($model);
     }
 
     public function deleted(Model $model)
     {
-        Queue::push('Menthol\Flexible\Jobs\ReindexJob', RelatedModelsDiscovery::getRelatedModels($model, $model->flexibleRelatedModels));
-        $model->flexibleRelatedModels = [];
+        $this->pushReindexJob($model);
     }
 
     public function restoring(Model $model)
     {
-        $model->flexibleRelatedModels = RelatedModelsDiscovery::getRelatedModels($model);
+        $this->observeRelatedModels($model);
     }
 
     public function restored(Model $model)
     {
-        Queue::push('Menthol\Flexible\Jobs\ReindexJob', RelatedModelsDiscovery::getRelatedModels($model, $model->flexibleRelatedModels));
-        $model->flexibleRelatedModels = [];
+        $this->pushReindexJob($model);
     }
 
 }
